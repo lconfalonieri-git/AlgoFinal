a887fc228ddc7e58a500015fb754484222245102
lconfalonieri-git <confalonieri.l@northeastern.edu>
2024-10-24 13:43:42 -0400

    Added function headers we may need as well as libraries

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+
+void simulateEvent(){
+
+}
+
+void updateTable(){
+
+}
+
+void display(){
+
+}
 
 int main(){
     cout << "Test" << endl;
